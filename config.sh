656e3dcb7dd8642254c5f8c26d682b2223bdf520
pt20912 <122719561+pt20912@users.noreply.github.com>
2023-03-11 20:49:15 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/AsrixAp/twrp_device_m51"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="samsung"
+export OEM="m51"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=Arixcuti:3 & Magic Dev Team"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
